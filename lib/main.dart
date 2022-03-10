1da918f0c355ee1c201dec90f047a9542ec99e54
苏晟 <shengsu15@gmail.com>
2022-03-10 10:42:09 +0800

    add unknownRoute

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       initialRoute: AppPages.initial,
+      unknownRoute: AppPages.unknownRoute,
       getPages: AppPages.routes,
     );
   }
