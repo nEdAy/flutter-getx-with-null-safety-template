faccf26d57dbd99fb5884013ba5d20627843a49c
Kevin SuTJ <shengsu15@gmail.com>
2022-04-07 17:21:54 +0800

    SafeArea

--- lib/pages/home/home_view.dart
+++ lib/pages/home/home_view.dart
@@ -15,63 +15,65 @@ class HomeView extends GetView<HomeController> {
         title: const Text('HomeView'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Obx(() => Text(
-                  '${controller.count}',
-                  style: Theme.of(context).textTheme.headline4,
-                )),
-            Card(
-              margin: const EdgeInsets.all(10.0),
-              child: Container(
-                height: 150,
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10.0),
-                child: ListTile(
-                  leading: Assets.images.flutterLogo.image(),
-                  title: Obx(() => Text(
-                        '${controller.hitokoto}',
-                        textAlign: TextAlign.left,
-                        softWrap: true,
-                        style: Theme.of(context).textTheme.bodyLarge,
-                      )),
-                  subtitle: Obx(() => Text(
-                        '—— 「${controller.from}」',
-                        textAlign: TextAlign.right,
-                        softWrap: false,
-                        overflow: TextOverflow.ellipsis,
-                        style: Theme.of(context).textTheme.bodyMedium,
-                      )),
-                  trailing: IconButton(
-                    icon: const Icon(Icons.refresh),
-                    color: Colors.blue,
-                    onPressed: () => controller.onRefresh(),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              Obx(() => Text(
+                    '${controller.count}',
+                    style: Theme.of(context).textTheme.headline4,
+                  )),
+              Card(
+                margin: const EdgeInsets.all(10.0),
+                child: Container(
+                  height: 150,
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(10.0),
+                  child: ListTile(
+                    leading: Assets.images.flutterLogo.image(),
+                    title: Obx(() => Text(
+                          '${controller.hitokoto}',
+                          textAlign: TextAlign.left,
+                          softWrap: true,
+                          style: Theme.of(context).textTheme.bodyLarge,
+                        )),
+                    subtitle: Obx(() => Text(
+                          '—— 「${controller.from}」',
+                          textAlign: TextAlign.right,
+                          softWrap: false,
+                          overflow: TextOverflow.ellipsis,
+                          style: Theme.of(context).textTheme.bodyMedium,
+                        )),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.refresh),
+                      color: Colors.blue,
+                      onPressed: () => controller.onRefresh(),
+                    ),
                   ),
                 ),
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                ElevatedButton(
-                    child: const Text('Show Notification'),
-                    onPressed: () =>
-                        BotToast.showSimpleNotification(title: "init")),
-                TextButton(
-                    child: const Text('Show Toast'),
-                    onPressed: () => BotToast.showText(text: "Text One")),
-                OutlinedButton(
-                  child: const Text('Jump Page'),
-                  onPressed: () => Get.toNamed('/next'),
-                ),
-              ],
-            ),
-          ],
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(
+                      child: const Text('Show Notification'),
+                      onPressed: () =>
+                          BotToast.showSimpleNotification(title: "init")),
+                  TextButton(
+                      child: const Text('Show Toast'),
+                      onPressed: () => BotToast.showText(text: "Text One")),
+                  OutlinedButton(
+                    child: const Text('Jump Page'),
+                    onPressed: () => Get.toNamed('/next'),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
