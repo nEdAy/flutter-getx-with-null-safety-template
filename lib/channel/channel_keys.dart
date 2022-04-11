bd57c01e506014f3c76bf7ee981fa11d6954d3bc
Kevin SuTJ <shengsu15@gmail.com>
2022-04-11 09:58:11 +0800

    fix channel(template)

--- lib/channel/channel_keys.dart
+++ lib/channel/channel_keys.dart
@@ -1,7 +1,7 @@
 class ChannelKeys {
   static const packageKey = 'com.sunac.flutter';
 
-  static const backToNative = packageKey + '/goBackToNative';
+  static const backToNative = packageKey + '/backToNative';
 
   static const goToNative = packageKey + '/goToNative';
 
