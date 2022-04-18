97d411c5265a7e7a5204754630ff99dd5ec2ace6
Kevin SuTJ <shengsu15@gmail.com>
2022-04-18 17:51:41 +0800

    add eba_alarm item list UI

--- lib/pages/eba/alarm/eba_alarm_view.dart
+++ lib/pages/eba/alarm/eba_alarm_view.dart
@@ -15,7 +15,7 @@ class EbaAlarmView extends GetView<EbaAlarmController> {
             style: TextStyle(color: Colors.black, fontSize: 18)),
         centerTitle: true,
         backgroundColor: Colors.white,
-        elevation: 0,
+        elevation: 0.5,
         iconTheme: const IconThemeData(color: Colors.black),
         leading: IconButton(
           onPressed: () => Get.back(),
@@ -30,6 +30,7 @@ class EbaAlarmView extends GetView<EbaAlarmController> {
                 width: 3,
                 color: Color(0xFFD97F00),
               )),
+          padding: const EdgeInsets.symmetric(horizontal: 20),
           labelColor: Colors.black,
           labelStyle:
               const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
@@ -43,61 +44,69 @@ class EbaAlarmView extends GetView<EbaAlarmController> {
       body: SafeArea(
         child: TabBarView(
           controller: controller.tabController,
-          children: [
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Expanded(
-                    child: ListView.separated(
-                      itemBuilder: (context, index) {
-                        return Container(
-                          height: 40,
-                          alignment: Alignment.center,
-                          child: Text(
-                            '$index',
-                            style: const TextStyle(fontSize: 20),
-                          ),
-                        );
-                      },
-                      separatorBuilder: (context, index) {
-                        return const Divider();
-                      },
-                      itemCount: 10,
-                    ),
-                  ),
-                  const Text('共52条记录'),
-                ],
-              ),
-            ),
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Expanded(
-                    child: ListView.separated(
-                      itemBuilder: (context, index) {
-                        return Container(
-                          height: 40,
-                          alignment: Alignment.center,
-                          child: Text(
-                            '$index',
-                            style: const TextStyle(fontSize: 20),
-                          ),
-                        );
-                      },
-                      separatorBuilder: (context, index) {
-                        return const Divider();
-                      },
-                      itemCount: 10,
-                    ),
-                  ),
-                  const Text('共52条记录'),
-                ],
-              ),
-            ),
-          ],
+          children: const [EbaAlarmItemView(), EbaAlarmItemView()],
         ),
+      ),
+    );
+  }
+}
+
+class EbaAlarmItemView extends GetView<EbaAlarmController> {
+  const EbaAlarmItemView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: ListView.separated(
+              itemBuilder: (context, index) {
+                return Container(
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 20),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '$index EBA监控设备-42：A8天台液低水位告警',
+                          style: const TextStyle(
+                              fontSize: 18,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w500),
+                        ),
+                        Container(height: 8),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              '$index 液位值<0.8',
+                              style: const TextStyle(
+                                  fontSize: 16, color: Color(0xFF434343)),
+                            ),
+                            Text(
+                              '$index 今天 16:13:25',
+                              style: const TextStyle(
+                                  fontSize: 14, color: Color(0xFFAAAAAA)),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+              separatorBuilder: (context, index) {
+                return const Divider(height: 1, indent: 20, endIndent: 20);
+              },
+              itemCount: 30,
+            ),
+          ),
+        ],
       ),
     );
   }
