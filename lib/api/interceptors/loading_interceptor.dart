e96a3954e46c374b15ec8c4dadd66a711b512d86
Kevin SuTJ <shengsu15@gmail.com>
2022-04-27 11:54:49 +0800

    fixed loading_interceptor bug

--- lib/api/interceptors/loading_interceptor.dart
+++ lib/api/interceptors/loading_interceptor.dart
@@ -2,24 +2,42 @@ import 'package:bot_toast/bot_toast.dart';
 import 'package:dio/dio.dart';
 
 class LoadingInterceptor extends InterceptorsWrapper {
-  late CancelFunc cancel;
+  final Map<String, CancelFunc> _cancelMap = {};
 
   @override
   void onRequest(
       RequestOptions options, RequestInterceptorHandler handler) async {
-    cancel = BotToast.showLoading(); //弹出一个加载动画
+    if (!options.headers.containsKey('noLoading') ||
+        options.headers['noLoading'] == false) {
+      CancelFunc cancel = BotToast.showLoading();
+      _cancelMap.addAll({options.uri.toString(): cancel});
+    }
     super.onRequest(options, handler);
   }
 
   @override
   void onResponse(Response response, ResponseInterceptorHandler handler) {
-    cancel();
+    cancelLoading(response.requestOptions);
     super.onResponse(response, handler);
   }
 
   @override
   void onError(DioError err, ErrorInterceptorHandler handler) {
-    cancel();
+    cancelLoading(err.requestOptions);
     super.onError(err, handler);
+  }
+
+  void cancelLoading(RequestOptions requestOptions) {
+    if (!requestOptions.headers.containsKey('noLoading') ||
+        requestOptions.headers['noLoading'] == false) {
+      final requestKey = requestOptions.uri.toString();
+      if (_cancelMap.containsKey(requestKey)) {
+        CancelFunc? cancel = _cancelMap[requestKey];
+        if (cancel != null) {
+          cancel();
+          _cancelMap.remove(requestKey);
+        }
+      }
+    }
   }
 }
