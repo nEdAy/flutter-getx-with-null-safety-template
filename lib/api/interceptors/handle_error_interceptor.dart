83545db720511663c2b0478203a04fb245b00f6f
Kevin SuTJ <shengsu15@gmail.com>
2022-05-07 12:20:40 +0800

    add logout 401/403

--- lib/api/interceptors/handle_error_interceptor.dart
+++ lib/api/interceptors/handle_error_interceptor.dart
@@ -1,18 +1,30 @@
+import 'package:bot_toast/bot_toast.dart';
 import 'package:dio/dio.dart';
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+
+import '../../channel/back_to_native.dart';
 
 class HandleErrorInterceptor extends InterceptorsWrapper {
   final String tokenHeader = 'access-token';
 
   @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    if (response.statusCode == 401 || response.statusCode == 403) {
+      BotToast.showText(text: "登录过期，请重新登录");
+      BackToNativeChannel.backToNative(isLogout: true);
+    } else if (response.statusCode != 200) {
+      BotToast.showText(text: "请求服务器异常");
+    }
+    super.onResponse(response, handler);
+  }
+
+  @override
   void onError(DioError err, ErrorInterceptorHandler handler) {
     if (err.response?.statusCode == 401 || err.response?.statusCode == 403) {
-      Get.defaultDialog(
-          title: '提示',
-          content: const Text('登录过期，请重新登录'),
-          onConfirm: () => Get.back());
-    } else {}
+      BotToast.showText(text: "登录过期，请重新登录");
+      BackToNativeChannel.backToNative(isLogout: true);
+    } else {
+      BotToast.showText(text: "请求服务器异常");
+    }
     super.onError(err, handler);
   }
 }
--- lib/pages/eba/report/eba_report_download.dart
+++ lib/pages/eba/report/eba_report_download.dart
@@ -3,19 +3,24 @@ import 'dart:io';
 import 'package:bot_toast/bot_toast.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
 
+import '../../../channel/back_to_native.dart';
 import '../../../channel/get_user_info.dart';
 import '../../../config/flavor.dart';
 
 class EbaReportDownload {
+  final _dio = Dio();
+
   Future<void> downloadXLSFile(String url, CancelToken token) async {
     CancelFunc cancelLoadingFun = BotToast.showLoading(crossPage: false);
     final options = await _getDownloadOptions();
     if (options == null) {
       cancelLoadingFun();
       BotToast.showText(text: "登录过期，请重新登录");
+      BackToNativeChannel.backToNative(isLogout: true);
       return;
     }
     final savePath = await _getSavePath();
@@ -33,14 +38,16 @@ class EbaReportDownload {
           }
         }
       });
-    } on DioError catch (e) {
+    } on DioError catch (error) {
       cancelLoadingFun();
-      if (CancelToken.isCancel(e)) {
+      if (CancelToken.isCancel(error)) {
         BotToast.showText(text: "下载已取消");
       } else {
+        Logger().e(error);
         BotToast.showText(text: "下载异常");
       }
-    } on Exception catch (e) {
+    } on Exception catch (error) {
+      Logger().e(error);
       cancelLoadingFun();
     }
   }
@@ -62,10 +69,21 @@ class EbaReportDownload {
   }
 
   Future<String> _getSavePath() async {
-    Directory tempDir = await getTemporaryDirectory();
-    String _tempPath = tempDir.path;
-    String savePath = '$_tempPath/$_idGenerator.xls';
+    String _tempPath = "";
+    if (Platform.isAndroid) {
+      _tempPath = "/sdcard/download/";
+    } else {
+      _tempPath = (await getApplicationDocumentsDirectory()).path;
+    }
+    String _id= _idGenerator();
+    String savePath = '$_tempPath/$_id.xls';
     return savePath;
+  }
+
+  String _idGenerator() {
+    final uKey = UniqueKey().toString();
+    final now = DateTime.now().microsecondsSinceEpoch.toString();
+    return uKey + now;
   }
 
   void _download(
@@ -75,16 +93,9 @@ class EbaReportDownload {
     Options? options,
     void Function(int, int)? onReceiveProgress,
   }) async {
-    final dio = Dio();
-    await dio.download(url, savePath,
+    await _dio.download(url, savePath,
         cancelToken: cancelToken,
         options: options,
         onReceiveProgress: onReceiveProgress);
-  }
-
-  String _idGenerator() {
-    final uKey = UniqueKey().toString();
-    final now = DateTime.now().microsecondsSinceEpoch.toString();
-    return uKey + now;
   }
 }
